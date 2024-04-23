03e9bf1127547b5c3aefa9a49ef92df1d4e39d6a
Marcos Vinícius Cavalcante <60756051+mvinicavalcante@users.noreply.github.com>
2024-04-22 22:09:30 -0300

    feat: reset password controller created (#2)

---
+++ src/modules/users/controllers/ResetPasswordController.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import ResetPasswordService from '../services/ResetPasswordService';
+
+export class ResetPasswordController {
+  async create(request: Request, response: Response): Promise<Response> {
+    const { password, token } = request.body;
+
+    const resetPassowrd = new ResetPasswordService();
+
+    await resetPassowrd.execute({
+      password,
+      token,
+    });
+
+    return response.status(204).json();
+  }
+}
--- src/modules/users/services/ResetPasswordService.ts
+++ src/modules/users/services/ResetPasswordService.ts
@@ -15,18 +15,24 @@ class ResetPasswordService {
     const usersRepository = getCustomRepository(UsersRepository);
     const userTokenRepository = getCustomRepository(UserTokensRepository);
     const userToken = await userTokenRepository.findByToken(token);
+
     if (!userToken) {
       throw new AppError('User Token does not exists.');
     }
+
     const user = await usersRepository.findById(userToken.id);
+
     if (!user) {
       throw new AppError('User does not exists.');
     }
+
     const tokenCreatedAt = userToken.created_at;
     const compareDate = addHours(tokenCreatedAt, 2);
+
     if (isAfter(Date.now(), compareDate)) {
       throw new AppError('Token expired.');
     }
+
     user.password = await hash(password, 8);
   }
 }

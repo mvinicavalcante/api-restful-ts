fc4e85f7acce320be4291e6bd570bf31906cbbcf
Marcos Vinícius Cavalcante <mvinic2011@gmail.com>
2024-04-22 21:53:26 -0300

    feat: forgot password

---
+++ src/modules/users/routes/password.routes.ts
@@ -0,0 +1,18 @@
+import { Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
+import ForgotPasswordController from '../controllers/ForgotPassword';
+
+const passwordRoute = Router();
+const forgotPasswordController = new ForgotPasswordController();
+
+passwordRoute.post(
+  '/forgot',
+  celebrate({
+    [Segments.BODY]: {
+      email: Joi.string().email().required(),
+    },
+  }),
+  forgotPasswordController.create,
+);
+
+export default passwordRoute;
--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,6 +1,7 @@
 import productsRouter from '@modules/products/routes/products.routes';
 import sessionsRoute from '@modules/users/routes/session.routes';
 import usersRoute from '@modules/users/routes/users.routes';
+import passwordRoute from '@modules/users/routes/password.routes';
 import { Router } from 'express';
 
 const routes = Router();
@@ -9,5 +10,6 @@ const routes = Router();
 routes.use('/products', productsRouter);
 routes.use('/users', usersRoute);
 routes.use('/sessions', sessionsRoute);
+routes.use('/password', passwordRoute);
 
 export default routes;
